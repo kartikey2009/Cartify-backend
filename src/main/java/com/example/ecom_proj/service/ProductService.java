13a699cb55331fcaf65f0e0710b4a95e70ceed4a
Kartikey Sharma <kartikey.sharma0320@gmail.com>
2026-04-26 16:45:22 +0530

    first commit

---
+++ src/main/java/com/example/ecom_proj/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.ecom_proj.service;
+
+import com.example.ecom_proj.model.Product;
+import com.example.ecom_proj.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+
+         return repo.findAll();   //it will return the list of products.
+
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+}
